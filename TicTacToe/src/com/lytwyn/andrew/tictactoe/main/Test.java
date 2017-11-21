e8b3b66c2d5673bbd8b94643973b7d798a1a2eaf
Andrew L <AndrewL@hexaware.com>
2017-11-21 11:25:08 -0500

    Test class (Hello World)

---
+++ TicTacToe/src/com/lytwyn/andrew/tictactoe/main/Test.java
@@ -0,0 +1,10 @@
+package com.lytwyn.andrew.tictactoe.main;
+
+/**
+ * Created by andrew on 11/21/2017.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
